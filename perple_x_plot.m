b34624c6aead540cbd6119c41b6b7f549ff76938
jamie <jamie@ethz.ch>
2011-04-16 11:07:28 +0200

    corrects perplex_plot script

--- perple_x_plot.m
+++ perple_x_plot.m
@@ -4,7 +4,7 @@
 
 clear all; clf;
 
-[x,y,a,xname,yname,zname,nvar,mvar,nrow,dnames,titl] = get_perple_x_file; %open the Perple_X file
+[x,y,a,xname,yname,zname,nvar,mvar,nrow,dnames,titl] = function_to_get_perple_x_file; %open the Perple_X file
 
 function_for_perple_x_plots (x,y,a,xname,yname,zname,nvar,mvar,nrow,dnames,titl);
 
