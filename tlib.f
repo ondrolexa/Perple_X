a37fdd8305ac392f1559178d370cd137c17c509a
jamie <jamie@ethz.ch>
2012-03-07 13:28:50 +0100

    increase k21 to 1e6, left k1 at 1e6 in perplex_parameters.h.

--- tlib.f
+++ tlib.f
@@ -17,7 +17,7 @@ c----------------------------------------------------------------------
 	implicit none
 
       write (*,'(/,a)') 
-     *      'Perple_X version 6.6.7, source updated March 3, 2012.'
+     *      'Perple_X version 6.6.7, source updated March 7, 2012.'
 
       end
 
